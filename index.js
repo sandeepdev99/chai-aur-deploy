d36499450f564a5ba495c3897e5992e1d19f4705
sandeepdev99 <sandeep99.dev@gmail.com>
2025-12-02 17:27:04 +0530

    completed firt basic deployable app.

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
+
+
